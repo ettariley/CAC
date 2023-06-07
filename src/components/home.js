607f5c56564b11f7f6f1861ca78f819274917122
Etta Baker <etta.r.baker@gmail.com>
2023-06-07 00:21:09 -0400

    worked on home page landing

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Fade from 'react-bootstrap/Fade';
+import Image from 'react-bootstrap/Image';
+import HomeLanding from '../assets/home-landing.png';
 
 function Home() {
   const [open, setOpen] = useState(false);
@@ -15,10 +17,23 @@ function Home() {
   return (
     <Fade in={open}>
       <Container className="mt-5 pt-5">
-        <h2>Home</h2>
-        <Row>
+        <Row className="mt-4">
+          <Col className="home-landing">
+            <Row>Hearing. Helping. Healing.</Row>
+          </Col>
+          {/* <Col md="7">
+            <Row>
+              <Col className="home-landing">Hearing.</Col>
+            </Row>
+            <Row>
+              <Col className="home-landing offset-md-2">Helping.</Col>
+            </Row>
+            <Row>
+              <Col className="home-landing offset-md-4">Healing.</Col>
+            </Row>
+          </Col> */}
           <Col>
-            <p>Content here</p>
+            <Image src={HomeLanding} fluid />
           </Col>
         </Row>
       </Container>

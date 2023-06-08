95264194b11f89c7a9d03c37c8087815240272b8
Etta Baker <etta.r.baker@gmail.com>
2023-06-07 21:46:52 -0400

    updated footer info

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,63 +3,83 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import { NavLink } from 'react-router-dom';
+import Logo from '../assets/cac-logo.png';
 
 function Footer() {
   return (
     <Container fluid className="text-bg-light bg-primary">
-      <Row className="p-3 justify-content-around">
-        <Col sm md="3" className="m-1 me-2">
+      <Row className="p-3 justify-content-center">
+        <Col sm md="2" className="me-2">
           <h4>Contact Us</h4>
-          <Row className='mb-2'>
-            <a href="mailto:cac3jd@gmail.com" title="Email" target="_blank">
-              cac3jd@gmail.com
-            </a>
+          <Row>
+            <p className="mb-1">
+              Email:{' '}
+              <a href="mailto:cac3jd@gmail.com" title="Email" target="_blank">
+                cac3jd@gmail.com
+              </a>
+            </p>
           </Row>
           <Row>
-            <a href="tel:423-422-4446" title="Call">
-              (423) 422-4446
-            </a>
+            <p className="mb-1">
+              Phone:{' '}
+              <a href="tel:423-422-4446" title="Call">
+                (423) 422-4446
+              </a>
+            </p>
           </Row>
-          <Row><p className='mb-2'>(423) 422-4499 (fax)</p></Row>
+          <Row>
+            <p className="mb-2">Fax: (423) 422-4499</p>
+          </Row>
+        </Col>
+        <Col sm md="2" className="me-2">
+          <h4>Address</h4>
+          <Row>
+            <p className="mb-2">
+              200 Main Street<br></br>Mosheim, TN 37818
+            </p>
+          </Row>
           <Row>
             <p>
+              Mailing:<br></br>
               PO Box 743<br></br>Mosheim, TN 37818
             </p>
           </Row>
         </Col>
-        <Col sm md="3" className="m-1">
-          <h4>Other Resources</h4>
+        <Col sm md="2" className="me-2">
+          <h4>Resources</h4>
           <p>
-            Report Child Abuse:{' '}
+            Report Child Abuse<br></br>
             <a href="tel:1-877-237-0004" title="Call">
               1-877-237-0004
             </a>
           </p>
-          <a href='https://www.nationalchildrensalliance.org/' target='_blank'>
-            <img
+          <a href="https://www.nationalchildrensalliance.org/" target="_blank">
+            <Image
               src={require('../assets/natl-childrens-alliance.png')}
               alt="National Children's Alliance Logo"
-              className='footer-images'
+              fluid
+              className="footer-images"
             />
-          </a> <br></br>
-          <a href='https://www.kidcentraltn.com/' target='_blank'>
-            <img
+          </a>{' '}
+          <br></br>
+          <a href="https://www.kidcentraltn.com/" target="_blank">
+            <Image
               src={require('../assets/kidcentral-tn.png')}
               alt="Kid Central TN Logo"
-              className='footer-images mt-2'
+              className="mt-2 footer-images"
+              fluid
             />
           </a>
         </Col>
-
-        <Col sm md="3" className="m-1 ms-2">
+        <Col sm md="2" className="">
           <h4>Site Menu</h4>
-          <Row>
+          <Row className="mb-1">
             <NavLink to="/">Home</NavLink>
           </Row>
-          <Row>
+          <Row className="mb-1">
             <NavLink to="/who-we-are">Who We Are</NavLink>
           </Row>
-          <Row>
+          <Row className="mb-1">
             <NavLink to="/what-we-do">What We Do</NavLink>
           </Row>
           <Row>
@@ -67,9 +87,12 @@ function Footer() {
           </Row>
         </Col>
       </Row>
-      <Row className='footer-site p-1'>
-        <Col className='text-center'>
-          Site Created by <a href='https://www.ettahaselden.com/' target='_blank'>Etta Haselden</a>
+      <Row className="footer-site p-1">
+        <Col className="text-center">
+          Site Created by{' '}
+          <a href="https://www.ettahaselden.com/" target="_blank">
+            Etta Haselden
+          </a>
         </Col>
       </Row>
     </Container>
